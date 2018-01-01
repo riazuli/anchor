4123509c9debb25ef70c0c1bdcd61d0df4299ea8
riazuli <ri64627@gmail.com>
2018-01-01 16:25:45 +0600

    init

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,12 +3,11 @@
 
     jQuery(document).ready(function($){
 
-        $(".homepage-slider").owlCarousel({
+        $(".hero-slider").owlCarousel({
             items:1,
             loop:true,
-            nav:true,
-            dots:false,
-            navText: ["<i class='fa fa-long-arrow-left'>","<i class='fa fa-long-arrow-right'>"],
+            nav:false,
+            dots:true,
             autoplay:true,
         });
 
